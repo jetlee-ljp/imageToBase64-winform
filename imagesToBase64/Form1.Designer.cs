dbd6bfaae5de067a87b09e1964142d0608dbe774
jetlee <itlijinpeng@outlook.com>
2020-05-13 19:06:49 +0800

    添加项目文件。

---
+++ imagesToBase64/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace imagesToBase64
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(396, 14);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "选择文件";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(12, 15);
+            this.txt1.Name = "txt1";
+            this.txt1.ReadOnly = true;
+            this.txt1.Size = new System.Drawing.Size(378, 21);
+            this.txt1.TabIndex = 1;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(13, 43);
+            this.txt2.Multiline = true;
+            this.txt2.Name = "txt2";
+            this.txt2.ReadOnly = true;
+            this.txt2.Size = new System.Drawing.Size(537, 341);
+            this.txt2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(477, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 23);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 396);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "图片转base64(markdown)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ imagesToBase64/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace imagesToBase64
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            // 创建文件选择控件对象
+            OpenFileDialog dialog = new OpenFileDialog();
+            // 禁止多选
+            dialog.Multiselect = false;
+            dialog.Title = "请选择要转换成base64的图片";
+
+            dialog.Filter = "All Image Files|*.bmp;*.ico;*.gif;*.jpeg;*.jpg;*.png;*.tif;*.tiff";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                txt1.Text = dialog.FileName;     //显示文件路径
+                FileInfo myFile = new FileInfo(dialog.FileName);
+                string fileExtension = Path.GetExtension(myFile.FullName);//即扩回展名答
+
+                switch (fileExtension)
+                {
+                    case ".png":
+                        txt2.Text = "data:image/png;base64," + ImgToBase64(dialog.FileName);
+                        break;
+                    case ".ico":
+                        txt2.Text = "data:image/x-icon;base64," + ImgToBase64(dialog.FileName);
+                        break;
+                    case ".gif":
+                        txt2.Text = "data:image/gif;base64," + ImgToBase64(dialog.FileName);
+                        break;
+                    default:
+                        txt2.Text = "data:image/jpeg;base64," + ImgToBase64(dialog.FileName);
+                        break;
+                }
+                button1.Text = "点击复制";
+                button1.Enabled = true;
+            }
+            else
+            {
+                txt1.Text = "";
+                txt2.Text = "";
+                button1.Text = "";
+                button1.Enabled = false;
+            }
+
+        }
+
+        public string ImgToBase64(string ImageFileName)
+        {
+            try
+            {
+                Bitmap bmp = new Bitmap(ImageFileName);
+
+                MemoryStream ms = new MemoryStream();
+                bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                byte[] arr = new byte[ms.Length];
+                ms.Position = 0;
+                ms.Read(arr, 0, (int)ms.Length);
+                ms.Close();
+                return Convert.ToBase64String(arr);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Clipboard.SetDataObject(txt2.Text);
+        }
+    }
+}
